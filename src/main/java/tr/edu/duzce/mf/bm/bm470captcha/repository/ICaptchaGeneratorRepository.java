784551c881877411251296619d2307aaaef71b0f
MelihEmreGuler <gulermelihemre@gmail.com>
2025-06-09 01:09:43 +0300

    refactor: clean up old project structure

--- src/main/java/tr/edu/duzce/mf/bm/bm470captcha/repository/ICaptchaGeneratorRepository.java
+++
@@ -1,8 +0,0 @@
-package tr.edu.duzce.mf.bm.bm470captcha.repository;
-
-
-import tr.edu.duzce.mf.bm.bm470captcha.entity.Captcha;
-
-public interface ICaptchaGeneratorRepository {
-    void save(Captcha captcha);
-}
--- src/main/java/tr/edu/duzce/mf/bm/bm470captcha/repository/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package tr.edu.duzce.mf.bm.bm470captcha.repository;
-
-import tr.edu.duzce.mf.bm.bm470captcha.entity.User;
-
-public interface IUserRepository {
-    User findByUsername(String username);
-    void save(User user);
-}
